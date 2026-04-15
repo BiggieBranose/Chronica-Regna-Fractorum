c148b7cdd3998b01b195dd76fae244f904156735
BiggieBranose <Johannesravnpaulsen@gmail.com>
2026-04-15 11:26:56 +0200

    Starting point for the suffering of JO hannes

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,74 +1,48 @@
-#include <memory>
 #if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
-#	include <vulkan/vulkan_raii.hpp>
+#include <vulkan/vulkan_raii.hpp>
 #else
 import vulkan_hpp;
 #endif
 #include <GLFW/glfw3.h>
 
-#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+#include <cstdlib>
 
-const uint32_t WIDTH  = 800;
-const uint32_t HEIGHT = 600;
+class HelloTriangleApplication {
+public:
+    void run() {
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
 
-class HelloTriangleApplication
-{
-  public:
-	void run()
-	{
-		initWindow();
-		initVulkan();
-		mainLoop();
-		cleanup();
-	}
+private:
+    void initVulkan() {
 
-  private:
-	GLFWwindow *window = nullptr;
+    }
 
-	void initWindow()
-	{
-		glfwInit();
+    void mainLoop() {
 
-		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    }
 
-		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-	}
+    void cleanup() {
 
-	void initVulkan()
-	{
-	}
-
-	void mainLoop()
-	{
-		while (!glfwWindowShouldClose(window))
-		{
-			glfwPollEvents();
-		}
-	}
-
-	void cleanup()
-	{
-		glfwDestroyWindow(window);
-
-		glfwTerminate();
-	}
+    }
 };
 
 int main()
 {
-	try
-	{
-		HelloTriangleApplication app;
-		app.run();
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
+    try
+    {
+        HelloTriangleApplication app;
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
