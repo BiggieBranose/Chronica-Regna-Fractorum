9af6a7595f8cf3df8d1290a6c06425d58dae0c08
BiggieBranose <johannesravnpaulsen@gmail.com>
2026-08-06 04:48:12 +0200

    feat(graphics): More error catching and conditional error catching

--- Code/engine/graphics/GlTFLoader.cpp
+++ Code/engine/graphics/GlTFLoader.cpp
@@ -15,10 +15,14 @@ namespace crf {
 
         if (!loader.LoadBinaryFromFile(&model, &err, &warn, filepath)) {
             crf::Log::error("Failed to load glTF scene from: {}", filepath);
+            crf::Log::error("Reason: {}", err);
             return;
         }
 
         crf::Log::info("glTF scene loaded successfully from: {}", filepath);
+        if (!warn.empty()) {
+            crf::Log::warn("Warnings: {}", warn);
+        }
         crf::Log::info("Scene: {} meshes, {} nodes, {} scenes (default: {})",
             model.meshes.size(), model.nodes.size(), model.scenes.size(),
             model.defaultScene);
