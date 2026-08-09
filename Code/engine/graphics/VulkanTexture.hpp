70e178c98b9965ec3d03833b19441dcf76438ce1
BiggieBranose <johannesravnpaulsen@gmail.com>
2026-08-09 17:06:08 +0200

    feat(graphics) added VulkanTexture.hpp to make a vkImage the gpu can read and assign to a model. only hpp for now

---
+++ Code/engine/graphics/VulkanTexture.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "core/Types.hpp"
+#include "VulkanContext.hpp"
+#include <string>
+
+namespace crf {
+
+class VulkanTexture {
+public:
+    VulkanTexture(VulkanContext& context, const std::string& filepath);
+    ~VulkanTexture();
+
+    VulkanTesture(const VulkanTexture&) = delete;
+    VulkanTexture& operator=(const VulkanTexture&) = delete;
+    VulkanTexture(VulkanTexture&&) = delete;
+    VulkanTexture& operator=(VulkanTexture&&) = delete;
+
+    VkImageView getImageView() const { return m_imageView; }
+    VkSampler getSampler() const { return m_sampler; }
+
+private:
+    void createImage(u32 widh, u32 height, VkFormat format, VkImageTiling tiling,
+                     VkImageUsageFlags usage, VkMemoryPropertyFlags properties,
+                     VkImage& image, VkDeviceMemory& imageMemory);
+    VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags);
+    void createSampler();
+
+    VulkanContext& m_context;
+    VkImage m_textureImage = nullptr;
+    VkDeviceMemory m_textureImageMemory = nullptr;
+    VkImageView m_textureImageView = nullptr;
+    VkSampler m_textureSampler = nullptr;
+};
+
+}
